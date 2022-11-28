d1faf2d14d557c6c9f6accd7b438cef7ab941f92
OtthoDogeru <118015923+OtthoDogeru@users.noreply.github.com>
2022-11-28 14:57:26 -0500

    Add files via upload

---
+++ javascript/alerta.js
@@ -0,0 +1,11 @@
+function mensaje(){
+	Swal.fire({
+		title: "La contraseña o el usuario es incorrecto",
+		text : "Verifica los datos",
+		icon: "error al ingresar"
+	});
+}
+
+function array(){
+	
+}
